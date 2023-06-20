242ddc26a487fdf4b6950ebee1aeaa90dade841e
МС 21 <m.reutsky@mail.ru>
2023-06-20 21:31:42 +0300

    first commit

---
+++ src/__tests__/health.js
@@ -0,0 +1,9 @@
+import healthLevel from '../health.js'
+test.each([
+    ['Маг', 60, 'healthy'],
+    ['Мечник', 22, 'wounded'],
+    ['Лучник', 10, 'critical'],
+  ])('testing level of health  with %s status and %i value', (_, health, status) => {
+    const result = healthLevel({ health });
+    expect(result).toBe(status);
+  });
