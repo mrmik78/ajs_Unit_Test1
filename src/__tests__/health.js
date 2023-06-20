c70d4228947773e0ee9a4b4870a172337236ecdb
МС 21 <m.reutsky@mail.ru>
2023-06-20 23:33:22 +0300

    fix eslint errors

--- src/__tests__/health.js
+++ src/__tests__/health.js
@@ -1,9 +1,10 @@
-import healthLevel from '../health.js'
+import healthLevel from '../health';
+
 test.each([
-    ['Маг', 60, 'healthy'],
-    ['Мечник', 22, 'wounded'],
-    ['Лучник', 10, 'critical'],
-  ])('testing level of health  with %s status and %i value', (_, health, status) => {
-    const result = healthLevel({ health });
-    expect(result).toBe(status);
-  });
+  ['Маг', 60, 'healthy'],
+  ['Мечник', 22, 'wounded'],
+  ['Лучник', 10, 'critical'],
+])('testing level of health  with %s status and %i value', (_, health, status) => {
+  const result = healthLevel({ health });
+  expect(result).toBe(status);
+});
--- src/health.js
+++ src/health.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line linebreak-style
 export default function healthLevel(lifeStatus) {
-    if (lifeStatus.health > 50) {
-        return 'healthy';
-    }
-    if (lifeStatus.health <= 50 && lifeStatus.health >= 15) {
-        return 'wounded';
-    }
-        return 'critical';
-    }
+  if (lifeStatus.health > 50) {
+    return 'healthy';
+  }
+  if (lifeStatus.health <= 50 && lifeStatus.health >= 15) {
+    return 'wounded';
+  }
+  return 'critical';
+}
